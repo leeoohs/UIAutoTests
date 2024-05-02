e3c36bfea2bd846c166eceb65a0283acdb1409e0
huangdaxianer <leeoohs@sina.com>
2024-05-02 21:58:40 +0800

    分支提交

--- httprunner_demo/utils/logging.py
+++ httprunner_demo/utils/logging.py
@@ -1,33 +1,33 @@
+import os
 import logging
+from datetime import datetime
 
 
-class TestCaseIndexTest:
-    # 这里省略了其他代码
+class Logger:
+    def __init__(self, name,  log_level=logging.DEBUG):
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(log_level)
 
-    def test_start(self):
-        # 设置 root logger 的日志级别为 ERROR
-        logging.basicConfig(level=logging.ERROR)
+        # 创建一个文件处理器，将日志写入到 logs 目录下的以时间命名的文件中
+        log_dir = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'logs')
+        os.makedirs(log_dir, exist_ok=True)
+        current_time = datetime.now().strftime("%Y%m%d%H%M%S")
+        log_file = os.path.join(log_dir, f"{name}_{current_time}.log")
+        file_handler = logging.FileHandler(log_file, encoding='utf-8')
+        file_handler.setLevel(logging.DEBUG)
 
-        for step in self.teststeps:
-            request = step.request
-            # 日志记录请求信息
-            logging.info(f"接口名称: {step.name}")
-            logging.info(f"接口地址: {request.url}")
-            logging.info(f"接口参数: {request.params}")
-            logging.info(f"请求头部: {request.headers}")
-            logging.info(f"请求cookies: {request.cookies}")
-            logging.info(f"请求数据: {request.data}")
+        # 创建一个控制台处理器，将日志输出到控制台
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.INFO)
 
-            response = request.run()  # 执行请求并获取响应数据
+        # 定义日志格式
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        file_handler.setFormatter(formatter)
+        console_handler.setFormatter(formatter)
 
-            # 过滤日志级别，只记录 error 级别的日志
-            if response.status_code != 200:
-                logging.error(f"请求失败，状态码：{response.status_code}")
-            else:
-                logging.info("请求成功")
+        # 将处理器添加到日志记录器中
+        self.logger.addHandler(file_handler)
+        self.logger.addHandler(console_handler)
 
-            # 日志记录响应信息
-            logging.info(f"响应数据: {response.text}")
-
-            # 断言逻辑
-            # 此处添加你的断言逻辑
+    def getLogger(self):
+        return self.logger
